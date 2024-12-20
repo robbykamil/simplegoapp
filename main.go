4be403b33126a92917f0cb518ba30d9777bc6722
Robby Kamil <71558401+robbykamil@users.noreply.github.com>
2024-12-20 15:02:42 +0700

    simplify main.go

--- main.go
+++ main.go
@@ -1,32 +1,19 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
 )
 
-//The root route
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my Go Application\n")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
-
-//The /health route
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status: %d\n", http.StatusOK)
-}
-
 
 func main() {
-	//Register the routes
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/health", healthHandler)
-
-	//Start the server
+	http.HandleFunc("/", handler)
 	port := "8080"
 	fmt.Printf("Server is running on port %s\n", port)
-	err := http.ListenAndServe(": " + port, nil)
-	if err != nil {
-		fmt.Printf("Error starting server %s", err)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Printf("Error: %v\n", err)
 	}
 }
